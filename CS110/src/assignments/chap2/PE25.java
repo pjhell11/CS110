3c31809c6e48af78fe84d51d8c96f3ee5bc898ba
Patrick Heller <PJ@140.190.156.122>
2013-01-22 19:04:52 -0600

    PE25 final

--- CS110/src/assignments/chap2/PE25.java
+++ CS110/src/assignments/chap2/PE25.java
@@ -13,10 +13,23 @@ public class PE25 {
 		double bill;
 		double tipRate;
 		double tip;
+		double total;
 		
 		Scanner s = new Scanner(System.in);
 		
+		System.out.println("Input the subtotal of the bill ");
 		
+		bill = s.nextDouble();
+		
+		System.out.println("Input the tip rate, in percent ");
+		
+		tipRate = s.nextDouble();
+		
+		tip = bill * (tipRate / 100);
+		
+		total = bill + tip;
+		
+		System.out.println("The tip is $" + tip + " and the total is $" + total);
 
 	}
 
