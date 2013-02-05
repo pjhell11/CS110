1cde279a12ddb3abacfbab2fb26d8a97227f9f01
Patrick Heller <PJ@140.190.121.60>
2013-02-04 20:02:21 -0600

    PE33

---
+++ CS110/src/assignments/chap3/PE33.java
@@ -0,0 +1,22 @@
+package assignments.chap3;
+
+public class PE33 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		boolean b = true;
+		i = (int)b;
+		
+		int i = 1;
+		boolean b = (boolean)i;
+		
+		//The answer to this question is "no." We get an error in both cases: "Cannot cast
+		//from boolean to int" and "Cannot cast from int to boolean" respectively.
+		
+	}
+
+}
