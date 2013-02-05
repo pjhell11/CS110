00c2c4e751c99e1e1bbf6494054043d59ce3c8f8
Patrick Heller <PJ@140.190.121.60>
2013-02-04 20:07:24 -0600

    PE 3.17

---
+++ CS110/src/assignments/chap3/PE317.java
@@ -0,0 +1,5 @@
+package assignments.chap3;
+
+//Yes. The else if assignment in the right box includes another condition, "income > 10000"
+//but this is redundant and unnecessary because the initial "if" condition already takes
+//care of it.
