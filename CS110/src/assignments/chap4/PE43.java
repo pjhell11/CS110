1234d7740a23a3cfe2b0e6c633b7b2ef50ba7e13
Patrick Heller <PJ@140.190.156.28>
2013-02-05 18:50:42 -0600

    PE 4.3

---
+++ CS110/src/assignments/chap4/PE43.java
@@ -0,0 +1,21 @@
+package assignments.chap4;
+
+public class PE43 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int max = 200;
+		System.out.println("Kilograms              Pounds");
+		for (float kg = 1; kg < max; kg++){
+			System.out.println("   " + kg + "                   " + kg*2.2);
+			kg++;
+			
+			//review pg 114 to finish the problem
+		}
+
+	}
+
+}
