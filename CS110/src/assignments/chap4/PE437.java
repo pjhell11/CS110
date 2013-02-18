e1fd786eeb45986e9af2733ff557000ea42f9e0c
Patrick Heller <PJ@140.190.120.67>
2013-02-17 20:16:25 -0600

    PE437

---
+++ CS110/src/assignments/chap4/PE437.java
@@ -0,0 +1,23 @@
+package assignments.chap4;
+
+import java.util.Scanner;
+
+public class PE437 {
+
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Input a number: ");
+		int d = s.nextInt();
+		int r;
+		String b = "";
+		for (int i=d; i>0; i = i/2){
+			r = d%2;
+			d = d/2;
+			b = r + b;
+			
+		}
+		System.out.println(b);
+	}
+
+}
