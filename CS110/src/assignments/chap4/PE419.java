47c8c28d31275c603549e23dc4664bc03b278bb9
Patrick Heller <PJ@140.190.156.10>
2013-02-19 07:50:36 -0600

    PE 4.19 resolved spacing issue.

--- CS110/src/assignments/chap4/PE419.java
+++ CS110/src/assignments/chap4/PE419.java
@@ -7,12 +7,12 @@ public class PE419 {
 		int i;
 		for (i=1; i<9; i++) {
 			for (int j = 8; j>=i; j--){
-				System.out.print("  ");
+				System.out.print("    ");
 			}
 			
 			int pwr = 0;
 			for (int k = 0; k < i; k++){
-				System.out.print("  " + (int)Math.pow(2, pwr));
+				System.out.printf("%4s", (int)Math.pow(2, pwr));
 				pwr++;
 			
 			}
@@ -21,7 +21,7 @@ public class PE419 {
 			for (int l = 0; l < i; l++){
 				pwr1--;
 				if ((int)Math.pow(2, pwr1) != 0)
-					System.out.print("  " + (int)Math.pow(2, pwr1));
+					System.out.printf("%4s", (int)Math.pow(2, pwr1));
 
 			}
 			
